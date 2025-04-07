053790a17c742abde22de03f31e10a1d99c68691
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2025-04-07 13:44:48 +0300

    Add AddressBook.Web WebAssembly project

---
+++ src/AddressBook.Web/AddressBookApiService.cs
@@ -0,0 +1,6 @@
+﻿namespace AddressBook.Web;
+
+public class AddressBookApiService
+{
+  
+}
---
+++ src/AddressBook.Web/Program.cs
@@ -0,0 +1,11 @@
+using AddressBook.Web;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new("http://localhost:5000") });
+
+await builder.Build().RunAsync();
