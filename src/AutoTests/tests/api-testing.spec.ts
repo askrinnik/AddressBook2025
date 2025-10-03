a5bee9f27040d311fd9ee8b3c5887fb158ab2fb5
Vira Skrynnik <veronskr@gmail.com>
2025-10-03 16:47:58 +0300

    Add API tests for Contacts endpoint and contact class implementation

---
+++ src/AutoTests/tests/api-testing.spec.ts
@@ -0,0 +1,85 @@
+import { test, expect } from '@playwright/test';
+import { describe } from 'node:test';
+import { Contact } from './class/contact';
+
+const apiUrl =
+  'https://addressbook-api-h5gmdghdcyfaf6gu.westeurope-01.azurewebsites.net/api/Contacts';
+
+describe('GET /api/Contacts', () => {
+  test('get all contacts', async ({ request }) => {
+    const response = await request.get(`${apiUrl}`);
+    console.log(await response.json());
+    expect(response.status()).toBe(200);
+  });
+
+  test('get all contacts by letters (first name)', async ({ request }) => {
+    const response = await request.get(`${apiUrl}?search=jo`);
+    console.log(await response.json());
+    expect(response.status()).toBe(200);
+  });
+
+  test('get all contacts by letters (last name)', async ({ request }) => {
+    const response = await request.get(`${apiUrl}?search=skr`);
+    console.log(await response.json());
+    expect(response.status()).toBe(200);
+  });
+});
+
+describe('GET /api/Contacts/{id}', () => {
+  test('get contact by id', async ({ request }) => {
+    const contactId = 1;
+    const response = await request.get(`${apiUrl}/${contactId}`);
+    const data = await response.json();
+    console.log(data);
+    expect.soft(response.status()).toBe(200);
+    expect.soft(data.id).toBe(contactId);
+  });
+
+  test('get contact by non-existed id', async ({ request }) => {
+    const contactId = 10000;
+    const response = await request.get(`${apiUrl}/${contactId}`);
+    expect(response.status()).toBe(404);
+  });
+});
+
+describe('POST /api/Contacts', () => {
+  test('create contact with birthday', async ({ request }) => {
+    const contact = Contact.createCorrectContactWithBirthday();
+    const response = await request.post(`${apiUrl}`, {
+      data: contact,
+    });
+    expect(response.status()).toBe(201);
+  });
+
+  test('create contact without birthday', async ({ request }) => {
+    const contact = Contact.createCorrectContactWithoutBirthday();
+    const response = await request.post(`${apiUrl}`, {
+      data: contact,
+    });
+    expect(response.status()).toBe(201);
+  });
+
+  test('create incorrect contact', async ({ request }) => {
+    const contact = Contact.createIncorrectContact();
+    const response = await request.post(`${apiUrl}`, {
+      data: contact,
+    });
+    expect(response.status()).toBe(400);
+  });
+});
+
+describe('DELETE /api/Contacts/{id}', () => {
+  //test('delete contact by id', async ({ request }) => {
+  //const contactId = 60;
+  //const response = await request.delete(`${apiUrl}/${contactId}`);
+  //expect.soft(response.status()).toBe(204);
+  //const responseAfterDeleting = await request.get(`${apiUrl}/${contactId}`);
+  //expect.soft(responseAfterDeleting.status()).toBe(404);
+  //});
+
+  test('delete contact by non-existed id', async ({ request }) => {
+    const contactId = 10000;
+    const response = await request.delete(`${apiUrl}/${contactId}`);
+    expect(response.status()).toBe(404);
+  });
+});
---
+++ src/AutoTests/tests/class/contact.ts
@@ -0,0 +1,23 @@
+export class Contact {
+  id: number;
+  firstName: string;
+  lastName: string;
+  birthday?: string;
+
+  constructor(firstName: string, lastName: string, birthday?: string) {
+    this.id = Math.floor(Math.random() * 1000) + 1;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = birthday;
+  }
+
+  static createCorrectContactWithBirthday(): Contact {
+    return new Contact('Petr', 'Petrov', '2011-11-11');
+  }
+  static createCorrectContactWithoutBirthday(): Contact {
+    return new Contact('Petr', 'Petrov');
+  }
+  static createIncorrectContact(): Contact {
+    return new Contact('1234567890123456789012345678901', '1234567890123456789012345678901');
+  }
+}
